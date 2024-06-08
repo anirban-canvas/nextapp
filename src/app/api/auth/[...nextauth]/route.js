67eee1fbb5d4da1c1222dc9d30f047f9ee3e2db0
anirban <codeelephant2@gmail.com>
2024-06-08 12:10:59 +0530

    initial commit 2

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,13 @@
 import GoogleProvider from 'next-auth/providers/google'
 import nextAuth from 'next-auth'
+import 'dotenv/config'
+console.log("Envs",process.env)
 const handler=nextAuth({
 
     providers:[
         GoogleProvider({
-            clientId:process.env.CLIENT_ID,
-            clientSecret:process.env.CLIENT_SECRET
+            clientId:"804552324082-81bqouh39kv41v94naslovbn2mlpqrbb.apps.googleusercontent.com",
+            clientSecret:"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"
         })
         
     ],
